853b117ce4af5f7d03374b4c751a459611b2d495
Kishan Doshi <kishan.doshi88@gmail.com>
2021-03-06 04:17:41 -0500

    hero image slider with dummy text

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,6 +4,11 @@
 const toggleMenu = document.querySelector('.toggle-menu');
 const menuToggle = document.querySelector('.menu-items');
 const menuItems = document.querySelectorAll('.menu-item');
+//slide
+const slides = document.querySelectorAll('.slide');
+const slideTitles = document.querySelectorAll('.slide .title');
+const slideDescs = document.querySelectorAll('.slide .desc');
+const slideNavs = document.querySelectorAll('.slide-nav');
 
 // Nav toggle and animations
 toggleMenu.addEventListener('click', () => {
@@ -22,3 +27,51 @@ toggleMenu.addEventListener('click', () => {
     }
 
 })
+
+
+// Slide animation
+let currentSlide = 0;
+const slideTo = index => {
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+    })
+
+    slideNavs.forEach(sn => {
+        sn.classList.remove('active');
+    })
+
+    if(index >= slides.length) {
+        index = currentSlide = 0;
+    }
+
+    
+    slides[index].classList.add('active');
+    slideNavs[index].classList.add('active');
+
+    // GSAP Animations
+    gsap.from(slideTitles[index], {
+        opacity: 0,
+        duration: 0.7,
+        y: 50,
+        delay: 0.2,
+    })
+
+    gsap.from(slideDescs[index], {
+        opacity: 0,
+        duration: 0.7,
+        y: 50,
+        delay: 0.5,
+    })
+}
+
+slideNavs.forEach((nav, i) => {
+    nav.addEventListener('click', () => {
+        slideTo(i);
+        currentSlide = i;
+    })
+})
+
+slideTo(0);
+// setInterval(() => {
+//     slideTo(++currentSlide);
+// }, 10000);

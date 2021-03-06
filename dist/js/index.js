2d02e0e429d986ac81cb9bca098bc74313c67f01
Kishan Doshi <kishan.doshi88@gmail.com>
2021-03-06 03:47:57 -0500

    navbar with animation

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,24 @@
-console.log('Hello, World');
+/********** DOM elements  **************/
+
+//nav
+const toggleMenu = document.querySelector('.toggle-menu');
+const menuToggle = document.querySelector('.menu-items');
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Nav toggle and animations
+toggleMenu.addEventListener('click', () => {
+    toggleMenu.classList.toggle('active');
+    menuToggle.classList.toggle('open');
+
+    if(menuToggle.classList.contains('open')) {
+        menuItems.forEach((menu, i) => {
+            gsap.from(menu, {
+                opacity: 0,
+                y: 100,
+                duration: 0.7,
+                delay: i*0.1,
+            })
+        })
+    }
+
+})

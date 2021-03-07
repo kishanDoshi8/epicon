31bbdfd01c8320b4029a5d0b8500210f023fc9dd
Kishan Doshi <kishan.doshi88@gmail.com>
2021-03-06 23:22:07 -0500

    audio/video demo with pricing cta section added

--- dist/js/index.js
+++ dist/js/index.js
@@ -9,6 +9,11 @@ const slides = document.querySelectorAll('.slide');
 const slideTitles = document.querySelectorAll('.slide .title');
 const slideDescs = document.querySelectorAll('.slide .desc');
 const slideNavs = document.querySelectorAll('.slide-nav');
+// demo section
+const videoOverlay = document.querySelector('.video-overlay');
+const video =  document.querySelector('video');
+const cursor = document.querySelector('.cursor');
+const speakers = document.querySelectorAll('.speakers');
 
 // Nav toggle and animations
 toggleMenu.addEventListener('click', () => {
@@ -75,3 +80,58 @@ slideTo(0);
 // setInterval(() => {
 //     slideTo(++currentSlide);
 // }, 10000);
+
+// Demo section
+videoOverlay.addEventListener('mouseover', () => {
+    document.addEventListener('mousemove', e => {
+        cursor.style.left = e.x + 'px';
+        cursor.style.top = e.y + 'px';
+    })
+})
+
+let isPause = false;
+cursor.addEventListener('click', () => {
+    if(isPause) {
+        isPause = false;
+        video.play();
+        cursor.children[0].remove();
+        let child = document.createElement("i");
+        child.classList.add("fas");
+        child.classList.add("fa-pause");
+        cursor.appendChild(child);
+    } else {
+        isPause = true;
+        video.pause();
+        cursor.children[0].remove();
+        let child = document.createElement("i");
+        child.classList.add("fas");
+        child.classList.add("fa-play");
+        cursor.appendChild(child);
+    }
+})
+
+speakers.forEach(speaker => {
+    speaker.addEventListener('click', () => {
+        if(video.muted) {
+            turnOnAudio();
+        } else {
+            turnOffAudio();
+        }
+    })
+})
+
+const turnOnAudio = () => {
+    video.muted = false;
+    speakers.forEach(speaker => {
+        speaker.classList.add('on');
+        speaker.classList.remove('off');
+    })
+}
+
+const turnOffAudio = () => {
+    video.muted = true;
+    speakers.forEach(speaker => {
+        speaker.classList.remove('on');
+        speaker.classList.add('off');
+    })
+}
